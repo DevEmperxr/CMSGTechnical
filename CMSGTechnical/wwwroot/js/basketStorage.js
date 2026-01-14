fb8e0d6d574580280a8a9a22357ad0fe2bdd08b2
DevEmperxr <emperorbizz@gmail.com>
2026-01-14 23:58:54 +0000

    feat: add localStorage init file

---
+++ CMSGTechnical/wwwroot/js/basketStorage.js
@@ -0,0 +1,12 @@
+﻿window.basketStorage = {
+    save: function (basket) {
+        localStorage.setItem('basket', JSON.stringify(basket));
+    },
+    load: function () {
+        const basket = localStorage.getItem('basket');
+        return basket ? JSON.parse(basket) : null;
+    },
+    clear: function () {
+        localStorage.removeItem('basket');
+    }
+};
